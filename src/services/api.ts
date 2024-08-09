96e8f3c7a23efdeac1fc0abad5bd185a7b428870
Lucas Souza <lucascorreia325@gmail.com>
2024-08-09 01:02:11 -0300

    fix: private key

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,10 +2,10 @@ import axios from 'axios';
 import md5 from 'md5';
 
 const publicKey = process.env.REACT_APP_API_KEY;
-const privateKey = process.env.REACT_APP_MARVEL_PRIVATE_KEY;
+const privateKey = '41feb6f27567fdf11a2fe7dcc3a85feac5f6e09b';
 
 const ts = new Date().getTime();
-const hash = md5(ts + "process.env.REACT_APP_MARVEL_PRIVATE_KEY" + publicKey);
+const hash = md5(ts + privateKey + publicKey);
 
 export default axios.create({
   baseURL: process.env.REACT_APP_BASE_URL,
